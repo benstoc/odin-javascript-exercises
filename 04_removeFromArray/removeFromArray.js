5c9aa3e864f3f64e487143717c3aa4684c638a56
Ben Stoc <179432173+benstoc@users.noreply.github.com>
2024-09-22 18:25:51 -0700

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    const newArray = [];
+    for (item of array) {
+        if (!items.includes(item)) {
+            newArray.push(item);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
