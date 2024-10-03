abfdf098300fd74be06e95680e38e4a04e2487f9
Ben Stoc <179432173+benstoc@users.noreply.github.com>
2024-10-03 16:04:08 -0700

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArr) {
+    //return bookArr.reduce((arr, book, index) => {
+    //    arr[index] = book.title;
+    //    return arr;
+    //}, [])
+    return bookArr.map(book => book.title)
 };
 
 // Do not edit below this line
