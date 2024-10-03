4206d324d2d4d397eb627aa1b1cbf47a9c026fc0
Ben Stoc <179432173+benstoc@users.noreply.github.com>
2024-10-03 15:25:42 -0700

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'
+const palindromes = function (string) {
+    string = string
+                .toLowerCase()
+                .split("")
+                .filter(item => alphanumerical.includes(item))
+                .join("");
+                
+    reverseString = string.split("").reverse().join("");
+    return (string === reverseString)
 };
 
 // Do not edit below this line
