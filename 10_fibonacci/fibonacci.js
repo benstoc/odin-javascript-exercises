40ea9e3f8a2ca0e10267d21de39e6828812a43b4
Ben Stoc <179432173+benstoc@users.noreply.github.com>
2024-10-03 15:52:56 -0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
 
+    const fibArr = [0, 1];
+    for (i = 1; i < n; i++) {
+        fibArr.push(fibArr[i - 1] + fibArr[i]);
+    }
+
+    return fibArr[n];
 };
 
 // Do not edit below this line
