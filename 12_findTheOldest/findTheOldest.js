eb4055cd7c1863688770a34280619446139de1e7
Ben Stoc <179432173+benstoc@users.noreply.github.com>
2024-10-03 18:37:16 -0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest1 = function(arr) {
+    return arr.sort((a, b) => {
+        if (!a.yearOfDeath || !b.yearOfDeath) {
+            return a.yearOfBirth - b.yearOfBirth;
+        };
 
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+        return bAge - aAge;
+    })[0];
 };
+
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+        if (!oldest.yearOfDeath) oldest.yearOfDeath = new Date().getFullYear();
+        if (!current.yearOfDeath) current.yearOfDeath = new Date().getFullYear();
+
+        const oldAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        const currAge = current.yearOfDeath - current.yearOfBirth;
+
+        return oldAge < currAge ? current : oldest;
+    })
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
